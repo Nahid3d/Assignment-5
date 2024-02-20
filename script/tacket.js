dab3df4a4c94d40ec9490f494f90db77755b4c63
Nahid islam <webdevlopernahid@gmail.com>
2024-02-20 19:02:19 +0600

    rechack

--- script/tacket.js
+++ script/tacket.js
@@ -29,30 +29,78 @@ let count = 8;
 let countSet= 0;
 for(const btn of allBtn){
     btn.addEventListener('click', function(e){  
+         console.log(btn)
 
         if(countSet <4 ){
             countSet = countSet + 1;
             setInnerText('left-seat',countSet);
         }
+       
         if( count > 0){
             count = count -1;
             setInnerText('t-count',count);
+
         }
+      
+
+         
+
 
         
          const price = e.target.parentNode.childNodes[1].innerText;
-         console.log(price)
-         const selectedCont = document.getElementById('select-seat');
+         const mainPrice =e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[4].parentNode.parentNode.childNodes[1].childNodes[5].childNodes[1].childNodes[9].childNodes[3].innerText;
+
+         const name = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[4].parentNode.parentNode.childNodes[1].childNodes[5].childNodes[1].childNodes[9].childNodes[5].childNodes[1].innerText;
+
+        
+
+         const seceltCentainerMain = document.getElementById('select-seat')
          const li = document.createElement('li');
+         const p1 = document.createElement('p');
+         p1.innerText = name;
+         const p2 = document.createElement('p');
+         p2.innerText = mainPrice;
+
+         li.appendChild(p1);
+         li.appendChild(p2);
+         seceltCentainerMain.appendChild(li);
+         
+
+         const selectedCont = document.getElementById('select-seat');
+         const h3 = document.createElement('h3');
          const p =document.createElement('p');
          p.innerText = price;
-         li.appendChild(p);
-         selectedCont.appendChild(li);
-         setInnerText(count)
+         h3.appendChild(p);
+         selectedCont.appendChild(h3);
+
+  
+         const totalAmount = document.getElementById('totalAmout').innerText;
+        const pricesConvartade =parseInt(mainPrice);
+        document.getElementById('select-seat').innerText
+         console.log(typeof pricesConvartade)
+         const convartadeAmount = parseInt(totalAmount);
+         document.getElementById('totalAmout').innerText =
+          convartadeAmount + parseInt(pricesConvartade);
+        console.log(typeof parseInt(totalAmount))
+
+
+        
+        const grandTotal = document.getElementById('grandTotal').innerText;
+        const grand =parseInt(mainPrice);
+        document.getElementById('select-seat').innerText
+         console.log(typeof pricesConvartade)
+         const convartadeGrand = parseInt(grandTotal);
+         document.getElementById('grandTotal').innerText =
+          convartadeGrand + parseInt(grand);
+        console.log(typeof parseInt(grandTotal))
+    
+         setInnerText(select-seat,count);
+
+
+       
+
 
     })
 }
-function setInnerText (id , value){
-    document.getElementById(id).innerText = value;
-}
+
 
---
+++ script/utilites.js
@@ -0,0 +1,43 @@
+function play(){
+    const playGroundSection = document.getElementById('play-ground');
+    playGroundSection.classList.remove('hidden');
+}
+
+function setInnerText (id , value){
+    document.getElementById(id).innerText = value;
+}
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+// btn 
+function handleClick1() {
+    document.getElementById("clickButton1").disabled = true;
+  }
+function handleClick2() {
+    document.getElementById("clickButton2").disabled = true;
+  }
+function handleClick3() {
+    document.getElementById("clickButton3").disabled = true;
+  }
+function handleClick4() {
+    document.getElementById("clickButton4").disabled = true;
+  }
+function handleClick5() {
+    document.getElementById("clickButton5").disabled = true;
+  }
+function handleClick6() {
+    document.getElementById("clickButton6").disabled = true;
+  }
+function handleClick7() {
+    document.getElementById("clickButton7").disabled = true;
+  }
+function handleClick8() {
+    document.getElementById("clickButton8").disabled = true;
+  }
+
+
+
+
+
